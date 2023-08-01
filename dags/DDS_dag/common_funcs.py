cec45d716e3753613a70b290bf43997c24abda2b
VladaYF <130729074+VladaYF@users.noreply.github.com>
2023-08-02 02:45:17 +0300

    Update common_funcs.py
    added email sender

--- dags/DDS_dag/common_funcs.py
+++ dags/DDS_dag/common_funcs.py
@@ -6,6 +6,9 @@ from psycopg2.extras import execute_values
 import pandas.io.sql as sqlio
 from sqlalchemy import create_engine
 import pandas as pd
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 
 def get_connect(conn_id):
@@ -49,3 +52,31 @@ def load_data(tables, error_tables, storage):
             df_error.to_sql(error_table, engine, schema='exceptions',
                             if_exists='append', index=False)
             print(f"LOADING SUCCESS {final_table}")
+
+def sendEmail(df):
+    email_password = 'xonrofnxaohndoxw'
+    email_sender = 'fexample898@gmail.com'
+    email_reciver = 'DAR_traineeship@korusconsulting.ru'
+
+    subject = "Малые остатки ходовых товаров"
+    
+    body = 'Елена, добрый день!\n\nЭто сообщение отправлено автоматически. Просьба не отвечать на него.\n\n\nНиже представлен список товаров, рекомендуемых к срочной закупке\n\n' + ';'.join(df.columns)
+
+    for i in df.values:
+        body += '\n' + str(i)
+    
+
+    body += '\n\n\nС Уважением,\nКоманда разработки Корус interns_1'
+    em = EmailMessage()
+
+    em['From'] = email_sender
+    em['To'] = email_reciver
+    em['subject'] = subject 
+
+    em.set_content(body)
+
+    context_em = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context_em) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_reciver, em.as_string())
+    print('Ура') 
