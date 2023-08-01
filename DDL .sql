1f5868be217fc061e7eaece3a936f8a789bd67a4
VladaYF <130729074+VladaYF@users.noreply.github.com>
2023-08-02 00:09:57 +0300

    Update DDL .sql

--- DDL .sql
+++ DDL .sql
@@ -1,4 +1,4 @@
-
+//dds слой
 
 CREATE TABLE IF NOT EXISTS brand (
     brand_id INT PRIMARY KEY,
@@ -26,13 +26,13 @@ CREATE TABLE IF NOT EXISTS stock (
     pos VARCHAR(255),
     available_quantity FLOAT,
     cost_per_item FLOAT,
-    PRIMARY KEY (available_on, product_id, pos, available_quantity),
+    PRIMARY KEY (product_id, pos, available_quantity),
     FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 CREATE TABLE transaction (
     transaction_id varchar,
     product_id int,
-    recorded_on timestamp,
+    recorded_on date,
     quantity float,
     price float,
     price_full float,
@@ -40,3 +40,83 @@ CREATE TABLE transaction (
     PRIMARY KEY (transaction_id, product_id),
     FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
+
+CREATE TABLE stores (
+    transaction_id varchar,
+    pos varchar)
+
+//data mart cлой
+
+CREATE TABLE sales (
+  transaction_id varchar,
+  product_id int,
+  name_short varchar,
+  quantity float,
+  price float,
+  price_full float,
+  cost_per_item float,
+  value_by_money float,
+  brand varchar,
+  category_name varchar,
+  recorded_on date,
+  pos varchar)
+
+
+CREATE TABLE IF NOT EXISTS stock (
+    product_id INT,
+    name_short varchar,
+    available_quantity FLOAT,
+    pos VARCHAR(255),
+    brand varchar,
+    category_name varchar,
+    cost_per_item FLOAT,
+    little_reserve bool);
+
+
+// exeptions layer 
+
+
+
+CREATE TABLE error_brand (
+    brand_id VARCHAR ,
+    brand VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS error_category (
+    category_id VARCHAR,
+    category_name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS error_product (
+    product_id VARCHAR,
+    name_short VARCHAR,
+    category_id VARCHAR,
+    pricing_line_id VARCHAR,
+    brand_id VARCHAR,
+    error VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS error_stock (
+    available_on VARCHAR,
+    product_id VARCHAR,
+    pos VARCHAR,
+    available_quantity VARCHAR,
+    cost_per_item VARCHAR,
+    error VARCHAR
+);
+CREATE TABLE error_transaction (
+    transaction_id VARCHAR,
+    product_id VARCHAR,
+    recorded_on VARCHAR,
+    quantity VARCHAR,
+    price VARCHAR,
+    price_full VARCHAR,
+    order_type_id VARCHAR,
+    error VARCHAR
+);
+
+CREATE TABLE stores (
+    transaction_id varchar,
+    pos varchar, 
+    error VARCHAR)
+
